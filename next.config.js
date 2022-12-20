3e2c26ff259789573bcb3d5d74f0ed9ecfee81bc
kohchenggee <kohchenggee@gmail.com>
2022-12-20 13:58:34 +0800

    add basePath

--- next.config.js
+++ next.config.js
@@ -6,10 +6,12 @@ const {
 } = process.env;
 
 const assetPrefix = ON_GITHUB_PAGES ? '/cg-web/' : '';
+const basePath = ON_GITHUB_PAGES ? '/cg-web' : '';
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix
+  assetPrefix,
+  basePath
 }
 
 module.exports = nextConfig
