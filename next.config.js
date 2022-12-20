f2fb851e604fd74a0b7817efe69d6e0e39f98f32
kohchenggee <kohchenggee@gmail.com>
2022-12-20 14:13:01 +0800

    remove basePath

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@
 const { ON_GITHUB_PAGES } = process.env;
 
 const assetPrefix = ON_GITHUB_PAGES ? "/cg-web/" : undefined;
-const basePath = ON_GITHUB_PAGES ? "/cg-web" : undefined;
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix,
-  basePath,
 };
 
 module.exports = nextConfig;
