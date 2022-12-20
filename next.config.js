3d88839c1c111b989579b14f14b62fab3fce7609
kohchenggee <kohchenggee@gmail.com>
2022-12-20 15:04:34 +0800

    image bugfix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const { ON_GITHUB_PAGES } = process.env;
-
-const assetPrefix = ON_GITHUB_PAGES ? "/cg-web/" : undefined;
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix,
+  assetPrefix:"/cg-web/",
 };
 
 module.exports = nextConfig;
