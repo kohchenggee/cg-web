680622b61f011b13444cf300205fe6f893ac31ad
kohchenggee <kohchenggee@gmail.com>
2022-12-20 14:08:21 +0800

    config error bugfix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 
 const { ON_GITHUB_PAGES } = process.env;
 
-const assetPrefix = ON_GITHUB_PAGES ? "/cg-web/" : "";
-const basePath = ON_GITHUB_PAGES ? "/cg-web" : "";
+const assetPrefix = ON_GITHUB_PAGES ? "/cg-web/" : undefined;
+const basePath = ON_GITHUB_PAGES ? "/cg-web" : undefined;
 
 const nextConfig = {
   reactStrictMode: true,
