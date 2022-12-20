ae881f608c4bd80772d41c95d9a6109a557acf58
kohchenggee <kohchenggee@gmail.com>
2022-12-20 13:51:30 +0800

    assetPrefix for github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+
+const {
+  ON_GITHUB_PAGES,
+} = process.env;
+
+const assetPrefix = ON_GITHUB_PAGES ? '/cg-web/' : '';
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix
 }
 
 module.exports = nextConfig
