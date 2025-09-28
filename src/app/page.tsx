c19c02cc97dec5860aead35fd52b64684e68d5f5
Cheng Gee <chenggee.koh@juliusbaer.com>
2025-09-28 14:20:12 +0800

    feat: Segregate mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ import { Canvas, useThree } from "@react-three/fiber";
 import { Environment, Html, Sphere, useProgress } from "@react-three/drei";
 import { Group, MeshPhysicalMaterial, type Object3DEventMap } from "three";
 import Camera from "./Camera";
+import Navbar from "@/lib/NavBar";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
@@ -120,7 +121,7 @@ export default function Home() {
       {
         heading: "Career",
         body: (
-          <div className="flex justify-center gap-8 py-8">
+          <div className="flex flex-wrap justify-center gap-8 py-8">
             <div className="mt-5">
               <JobCard
                 project={{
@@ -163,7 +164,7 @@ export default function Home() {
       {
         heading: "Project",
         body: (
-          <div className="flex justify-center gap-5 py-8">
+          <div className="flex flex-wrap justify-center gap-5 py-8">
             <div className="mt-5">
               <JobCard
                 project={{
@@ -385,136 +386,165 @@ export default function Home() {
           </div>
         </DialogContent>
       </Dialog>
-      {/* <Modal isOpen={open} toggle={() => setOpen(false)}>
-          <div>
-            <h3 className={styles.projectTitle}>Project Video</h3>
-            <div className={styles.modalContainer}>
-              <ReactPlayer
-                url={modalUrl}
-                className={styles.reactPlayer}
-                width="100%"
-                playing
-              />
-            </div>
-          </div>
-        </Modal> */}
       <title>Koh Cheng Gee</title>
       <meta name="description" content="Self Intro" />
       <link rel="icon" href="/favicon.ico" />
 
-      <main className="min-h-[calc(100vh)] flex flex-col justify-center items-center flex-1">
-        <Canvas
-          className="!fixed inset-0"
-          gl={{
-            alpha: false,
-            antialias: false,
-            powerPreference: "high-performance",
-          }}
-        >
-          <Suspense fallback={<Loader />}>
-            <ambientLight intensity={0.5} color="#fff" />
-            {/* Image downloaded from: https://polyhaven.com/hdris */}
-            <Environment
-              files={`${prefix}/assets/puresky.jpg`}
-              background={true}
-              resolution={512}
-              ground={false}
-            />
-            <ScrollingGroup />
-            <FixedGroup />
-            <Camera />
-          </Suspense>
-        </Canvas>
+      <div className="hidden md:block">
+        <main className="min-h-[calc(100vh)] flex flex-col justify-center items-center flex-1">
+          <Canvas
+            className="!fixed inset-0"
+            gl={{
+              alpha: false,
+              antialias: false,
+              powerPreference: "high-performance",
+            }}
+          >
+            <Suspense fallback={<Loader />}>
+              <ambientLight intensity={0.5} color="#fff" />
+              {/* Image downloaded from: https://polyhaven.com/hdris */}
+              <Environment
+                files={`${prefix}/assets/puresky.jpg`}
+                background={true}
+                resolution={512}
+                ground={false}
+              />
+              <ScrollingGroup />
+              <FixedGroup />
+              <Camera />
+            </Suspense>
+          </Canvas>
 
-        {loaded && (
-          <div className="relative z-50 w-full">
-            {SECTIONS.map((content, i) => (
-              <Section key={i} {...content} index={i} />
-            ))}
-          </div>
-        )}
-        {/* <Separator />
-        <section className="text-center w-full  py-2 px-4">
-          <h2 className="text-2xl">Career</h2>
-          <div className="flex justify-center gap-8 py-8">
-            <div className="mt-5">
-              <JobCard
-                project={{
-                  name: "Cognizant Technology Solutions",
-                  url: "https://www.cognizant.com/",
-                }}
-                name="Associate (Frontend)"
-                year="Mar 2023 - Present"
-              />
+          {loaded && (
+            <div className="relative z-50 w-full">
+              {SECTIONS.map((content, i) => (
+                <Section key={i} {...content} index={i} />
+              ))}
             </div>
-            <div className="mt-5">
-              <JobCard
-                project={{ name: "99.co", url: "https://www.99.co/" }}
-                name="Frontend Software Engineer"
-                year="Dec 2020 - Dec 2022"
-              />
+          )}
+        </main>
+      </div>
+      <div className="block md:hidden">
+        <Navbar />
+        <main className="min-h-[calc(100vh-68px)] flex flex-col justify-center items-center flex-1">
+          <h1 className={styles.title}>
+            <span className="animate-flip relative inline-block text-purple-500 [animation-delay:calc(0.2s*0)]">
+              C
+            </span>
+            <span className="animate-flip relative inline-block text-purple-700 [animation-delay:calc(0.2s*1)]">
+              G
+            </span>
+          </h1>
+          <Image
+            src={`${prefix}/assets/profile_img.png`}
+            alt="Profile Photo"
+            className={styles.profilePhoto}
+            width={100}
+            height={100}
+          />
+          <h2>
+            Hi, I am{" "}
+            <span className={styles.title_word + " " + styles.title_word_1}>
+              Koh{" "}
+            </span>
+            <span className={styles.title_word + " " + styles.title_word_2}>
+              Cheng{" "}
+            </span>
+            <span className={styles.title_word + " " + styles.title_word_3}>
+              Gee
+            </span>
+          </h2>
+
+          <p className={styles.description}>
+            I am a software engineer based in Singapore.
+          </p>
+
+          <Separator />
+          <section className="text-center w-full  py-2 px-4">
+            <h2 className="text-2xl">Career</h2>
+            <div className="justify-center gap-8 py-8">
+              <div className="mt-5">
+                <JobCard
+                  project={{
+                    name: "Cognizant Technology Solutions",
+                    url: "https://www.cognizant.com/",
+                  }}
+                  name="Associate (Frontend)"
+                  year="Mar 2023 - Present"
+                />
+              </div>
+              <div className="mt-5">
+                <JobCard
+                  project={{ name: "99.co", url: "https://www.99.co/" }}
+                  name="Frontend Software Engineer"
+                  year="Dec 2020 - Dec 2022"
+                />
+              </div>
+              <div className="mt-5">
+                <JobCard
+                  project={{
+                    name: "Streetsine",
+                    url: "https://www.srx.com.sg/",
+                  }}
+                  name="Mobile App Developer"
+                  year="May 2018 - Dec 2020"
+                />
+              </div>
             </div>
-            <div className="mt-5">
-              <JobCard
-                project={{ name: "Streetsine", url: "https://www.srx.com.sg/" }}
-                name="Mobile App Developer"
-                year="May 2018 - Dec 2020"
-              />
+          </section>
+          <Separator />
+          <section className="text-center w-full  py-2 px-4">
+            <h2 className="text-2xl">Project</h2>
+            <div className="justify-center gap-5 py-8">
+              <div className="mt-5">
+                <JobCard
+                  project={{
+                    name: "Jobify",
+                    url: "https://tutorial-jobify-dribrwuq5-kohchenggees-projects.vercel.app/",
+                  }}
+                  year="Dec 2024 - Jan 2025"
+                  list={[
+                    "NextJs",
+                    "Nx Monorepo",
+                    "Prisma",
+                    "TailwindCss",
+                    "Shadcn",
+                  ]}
+                />
+              </div>
+              <div className="mt-5">
+                <JobCard
+                  project={{
+                    name: "The Chosen One",
+                    url: "",
+                    onClick: () => {
+                      setOpen(true);
+                      setModalUrl("https://vimeo.com/782384898");
+                    },
+                  }}
+                  year="Aug 2017 - Nov 2017"
+                  list={["Unity"]}
+                />
+              </div>
+              <div className="mt-5">
+                <JobCard
+                  project={{
+                    name: "Trick Tower",
+                    url: "",
+                    onClick: () => {
+                      setOpen(true);
+                      setModalUrl("https://vimeo.com/782427046");
+                    },
+                  }}
+                  year="May 2018 - Dec 2020"
+                  list={["Unreal Engine"]}
+                />
+              </div>
             </div>
-          </div>
-        </section>
-        <Separator />
-        <section className="text-center w-full  py-2 px-4">
-          <h2 className="text-2xl">Project</h2>
-          <div className="flex justify-center gap-5 py-8">
-            <div className="mt-5">
-              <JobCard
-                project={{
-                  name: "Jobify",
-                  url: "https://tutorial-jobify-dribrwuq5-kohchenggees-projects.vercel.app/",
-                }}
-                year="Dec 2024 - Jan 2025"
-                list={[
-                  "NextJs",
-                  "Nx Monorepo",
-                  "Prisma",
-                  "TailwindCss",
-                  "Shadcn",
-                ]}
-              />
-            </div>
-            <div className="mt-5">
-              <JobCard
-                project={{
-                  name: "The Chosen One",
-                  url: "",
-                  onClick: () => {
-                    setOpen(true);
-                    setModalUrl("https://vimeo.com/782384898");
-                  },
-                }}
-                year="Aug 2017 - Nov 2017"
-                list={["Unity"]}
-              />
-            </div>
-            <div className="mt-5">
-              <JobCard
-                project={{
-                  name: "Trick Tower",
-                  url: "",
-                  onClick: () => {
-                    setOpen(true);
-                    setModalUrl("https://vimeo.com/782427046");
-                  },
-                }}
-                year="May 2018 - Dec 2020"
-                list={["Unreal Engine"]}
-              />
-            </div>
-          </div>
-        </section>
-        <Separator /> */}
-      </main>
+          </section>
+          <Separator />
+        </main>
+      </div>
 
       <footer className="mt-4 flex flex-wrap gap-4 justify-center">
         <span>
